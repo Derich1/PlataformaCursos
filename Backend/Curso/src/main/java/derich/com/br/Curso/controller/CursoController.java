a187db7df8706fd8187875c5e08d34426565a0cb
Derich Rosario <derich.rosario22@gmail.com>
2025-07-01 14:07:23 -0300

    observabilidade com tracing logging e metricas

--- Backend/Curso/src/main/java/derich/com/br/Curso/controller/CursoController.java
+++ Backend/Curso/src/main/java/derich/com/br/Curso/controller/CursoController.java
@@ -6,9 +6,10 @@ import derich.com.br.Curso.DTO.CursoDTO;
 import derich.com.br.Curso.DTO.CursoEditDTO;
 import derich.com.br.Curso.entity.Curso;
 import derich.com.br.Curso.service.CursoService;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +21,31 @@ import java.util.List;
 @RequestMapping("/curso")
 public class CursoController {
 
-    @Autowired
     private CursoService cursoService;
 
+    private final Tracer tracer;
+
     private static final Logger logger = LoggerFactory.getLogger(CursoController.class);
+
+    public CursoController(Tracer tracer, CursoService cursoService) {
+        this.tracer = tracer;
+        this.cursoService = cursoService;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("Tracer impl = " + tracer.getClass().getName());
+        Span span = tracer.nextSpan().name("manual-span").start();
+        try (Tracer.SpanInScope ws = tracer.withSpan(span)) {
+            span.tag("meu-teste", "micrometer-jaeger");
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            span.error(e);
+        } finally {
+            span.end();
+        }
+        return "Span gerado";
+    }
 
     @GetMapping
     private List<Curso> listarCursos () {
