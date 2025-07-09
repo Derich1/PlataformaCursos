60fd87573b3f688b253e7e33c09f372617cc0019
Derich Rosario <derich.rosario22@gmail.com>
2025-07-09 14:48:07 -0300

    fix(curso-controller) endpoint /criarPagamento stripe usar ApiKey stripe

--- Frontend/src/Pages/PagamentoStripe/index.tsx
+++ Frontend/src/Pages/PagamentoStripe/index.tsx
@@ -3,7 +3,8 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { ComprarCurso } from '../ComprarCurso';
 
-const stripePromise = loadStripe('pk_test_51Qx7AiFDDwihPybqNan3a0nMOmNHeVuabPQRYet4zXp5JH2RuAFjkKrZzVt572yxjJohhdwWQHRJk1FpG7QMfhOc00H1Pc45IF');
+const stripeKey = import.meta.env.STRIPE_KEY
+const stripePromise = loadStripe(stripeKey);
 
 export default function PagamentoStripe () {
   return (
