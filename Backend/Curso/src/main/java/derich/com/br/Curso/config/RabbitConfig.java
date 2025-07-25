b22c74d598a935a91e7b980bd74eb07389ce9b49
Derich Rosario <derich.rosario22@gmail.com>
2025-07-25 08:53:27 -0300

    fix(compra) processo de comprar

--- Backend/Curso/src/main/java/derich/com/br/Curso/config/RabbitConfig.java
+++ Backend/Curso/src/main/java/derich/com/br/Curso/config/RabbitConfig.java
@@ -2,6 +2,9 @@ package derich.com.br.Curso.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,18 @@ public class RabbitConfig {
     public static final String EMAIL_EXCHANGE = "exchange.email";
     public static final String EMAIL_QUEUE = "fila.email";
     public static final String EMAIL_ROUTING = "routing.email";
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
 
     @Bean
     public Exchange emailExchange() {
